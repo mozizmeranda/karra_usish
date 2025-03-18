0fa73cdc13035583b340dac3b1c53042b4809cea
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2025-03-18 21:36:04 +0500

    from rus to end

--- add_to_database.py
+++ add_to_database.py
@@ -5,17 +5,17 @@ cursor = conn.cursor()
 
 with open("add_to_db.txt", "r", encoding="utf-8") as file:
     for line in file:
-        parts = line.strip().split(" ")  # Разделяем строку по пробелам
+        parts = line.strip().split(" ")
         if len(parts) >= 3:
-            user_id = int(parts[0])  # ID
-            name = parts[1]  # Имя
-            phone = parts[2]  # Номер телефона
+            user_id = int(parts[0])
+            name = parts[1]
+            phone = parts[2]
 
-            # Добавляем пользователя в базу данных (если его ещё нет)
+
             cursor.execute("INSERT OR IGNORE INTO users (id, name, phone) VALUES (?, ?, ?)",
                            (user_id, name, phone))
 
 conn.commit()
 conn.close()
 
-print("Данные успешно добавлены в базу!")
+print("Saved :)")
