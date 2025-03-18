82748acb32158c59afc891ec93ca65e8ab55a24f
mozizmeranda <112464452+mozizmeranda@users.noreply.github.com>
2025-03-18 21:24:48 +0500

    adding users

---
+++ add_to_database.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+with open("users.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        parts = line.strip().split(" ")  # Разделяем строку по пробелам
+        if len(parts) >= 3:
+            user_id = int(parts[0])  # ID
+            name = parts[1]  # Имя
+            phone = parts[2]  # Номер телефона
+
+            # Добавляем пользователя в базу данных (если его ещё нет)
+            cursor.execute("INSERT OR IGNORE INTO users (id, name, phone) VALUES (?, ?, ?)",
+                           (user_id, name, phone))
+
+conn.commit()
+conn.close()
+
+print("Данные успешно добавлены в базу!")
